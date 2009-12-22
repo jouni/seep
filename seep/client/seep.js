fadc97032a82344c843873b59611fb92cde10947
Jouni Koivuviita <jouni@jounikoivuviita.com>
2009-12-23 00:35:22 +0200

    -

--- seep/client/seep.js
+++ seep/client/seep.js
@@ -1,10 +1,14 @@
-var seep = {};
+$LAB
+.script("../.json_parse.js").wait(function() {
+	var seep = {};
+	
+	seep.init = function(json) {
+		var ui = json_parse(json);
+		console.log(ui);
+	}
+	
+	window.onload = function() {
+		seep.init(window.seepinit);
+	}
+});
 
-seep.init = function(json) {
-	var ui = json_parse(json);
-	console.log(ui);
-}
-
-window.onload = function() {
-	seep.init(window.seepinit);
-}
